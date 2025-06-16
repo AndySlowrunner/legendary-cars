aa139bc52d3fe8e4d444af54ddc5a0abf505c124
Andy <andyfartushniy@gmail.com>
2025-06-16 23:17:47 +0300

    added header

--- legendary-cars/src/App.tsx
+++ legendary-cars/src/App.tsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react'
+import { Header } from './Header';
 
 import './App.css'
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <Header></Header>
         
     </>
   )
---
+++ legendary-cars/src/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+        <>
+            <p>Legendary Cars</p>
+            <nav>
+                <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Collection</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
